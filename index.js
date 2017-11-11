56b7499279c9eb276f6bc5998289357c75635704
Tristan Homsi <tristanhomsi@gmail.com>
2017-11-11 01:10:08 -0500

    add rule to prohibit calls of .send

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
 		"no-inline-assembly": require("./rules/no-inline-assembly"),
 		"no-call-value": require("./rules/no-call-value"),
 		"no-block-members": require("./rules/no-block-members"),
+		"no-send": require("./rules/no-send"),
 		"enforce-explicit-visibility": require("./rules/enforce-explicit-visibility")
 	}
 
