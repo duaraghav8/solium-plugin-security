c26bac20a922030372e38ef199f2f3c110d5bcff
Nicolas Feignon <nfeignon@gmail.com>
2017-11-12 21:49:20 +0100

    Add no-named-returns rule

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
 		"no-inline-assembly": require("./rules/no-inline-assembly"),
 		"no-call-value": require("./rules/no-call-value"),
 		"no-block-members": require("./rules/no-block-members"),
+		"no-named-returns": require("./rules/no-named-returns"),
 		"enforce-explicit-visibility": require("./rules/enforce-explicit-visibility")
 	}
 
