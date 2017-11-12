5b58eee42b2cab3529fb72f36f6819e438a0d982
Mitchell Van Der Hoeff <mitchell.vanderhoeff@gmail.com>
2017-11-12 14:34:00 -0500

    Add tests & check for returns not in functions

--- rules/enforce-single-end-return.js
+++ rules/enforce-single-end-return.js
@@ -1,6 +1,7 @@
 /**
  * @fileoverview Functions must have a single return at the end of the function.
  * @author Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
+ * @author Simon Hajjar <simon.j.hajjar@gmail.com>
  */
 
 'use strict';
@@ -32,11 +33,12 @@ module.exports = {
             if (body.length === 0) {
                 return;
             }
+            let function_name = node.name ? `"${node.name}"` : 'Fallback function';
             for (let statement of body.slice(0, -1)) {
                 if (statement['type'] === 'ReturnStatement') {
                     context.report({
                         node: node,
-                        message: 'Return statement not at end of function',
+                        message: `${function_name}: Return statement not at end of function`,
                     });
                 }
             }
@@ -44,30 +46,33 @@ module.exports = {
             if (last_statement['type'] !== 'ReturnStatement') {
                 context.report({
                     node: node,
-                    message: 'Missing return statement at end of function'
+                    message: `${function_name}: Missing return statement at end of function`
                 });
             }
         }
 
-        function inspectReturnDeclaration(emitted) {
+        function inspectBlockStatement(emitted) {
             var node = emitted.node;
 
             if (emitted.exit) {
                 return;
             }
 
-            let parent_node = context.getSourceCode().getParent(node);
-            if (parent_node['type'] !== 'FunctionDeclaration') {
-                context.report({
-                    node: node,
-                    message: 'Missing return statement at end of function'
-                });
+            if (node.parent.type !== 'FunctionDeclaration') {
+                for (let statement of node.body) {
+                    if (statement.type === 'ReturnStatement') {
+                        context.report({
+                            node: node,
+                            message: 'Missing return statement at end of function'
+                        });
+                    }
+                }
             }
         }
 
         return {
             FunctionDeclaration: inspectFunctionDeclaration,
-            ReturnStatement: inspectReturnDeclaration
+            BlockStatement: inspectBlockStatement
         };
 
     }
