5bcf240eacb898e2483649cf8c6b8c4be5c3848c
Raghav Dua <duaraghav8@gmail.com>
2017-11-14 13:55:25 +0530

    23 => 24

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ describe("Check Plugin's exported object", () => {
 		meta.description.should.be.type("string");
 
 		rules.should.be.type("object");
-		rules.should.have.size(23);
+		rules.should.have.size(24);
 
 		for (let ruleName in rules) {
 			rules[ruleName].should.be.type("object");
