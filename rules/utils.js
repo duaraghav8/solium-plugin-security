79db1c98abda9fa1f4473c4366451fa1754910fe
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-11-17 00:05:51 -0500

    refactored utils

--- rules/utils.js
+++ rules/utils.js
@@ -3,51 +3,60 @@
  * @author Simon Hajjar <simon.j.hajjar@gmail.com>
  */
 
+/**
+ * @param {Object} node The node to check
+ * @param {String} type The type of the node
+ * @returns {Boolean} true if the given node is the right type
+ */
+function isNodeType (node, name) {
+  return node['type'] === name;
+}
+
 module.exports = {
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is an BreakStatement
+   * @returns {Boolean} true if the given node is an BreakStatement
    */
   isBreak: function (node) {
-    return node['type'] === 'BreakStatement';
+    return isNodeType(node, 'BreakStatement');
   },
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is an ExpressionStatement
+   * @returns {Boolean} true if the given node is an ExpressionStatement
    */
   isExpression: function (node) {
-    return node['type'] === 'ExpressionStatement';
+    return isNodeType(node, 'ExpressionStatement');
   },
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is an AssignmentStatement
+   * @returns {Boolean} true if the given node is an AssignmentStatement
    */
   isAssignment: function (node) {
-    return node['type'] === 'AssignmentExpression';
+    return isNodeType(node, 'AssignmentExpression');
   },
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is an UpdateExpression
+   * @returns {Boolean} true if the given node is an UpdateExpression
    */
   isUpdate: function (node) {
-    return node['type'] === 'UpdateExpression';
+    return isNodeType(node, 'UpdateExpression');
   },
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is an IfStatement
+   * @returns {Boolean} true if the given node is an IfStatement
    */
   isIfStatement: function (node) {
-    return node['type'] === 'IfStatement';
+    return isNodeType(node, 'IfStatement');
   },
 
   /**
    * @param {Object} node The node to check
-   * @returns {Boolean} isObject true if the given node is a type of loop statement
+   * @returns {Boolean} true if the given node is a type of loop statement
    */
   isLoopStatement: function (node) {
     return ['ForStatement', 'WhileStatement', 'DoWhileStatement'].indexOf(node['type']) >= 0;
