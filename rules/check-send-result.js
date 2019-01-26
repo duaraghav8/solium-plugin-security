a95d3eabcf39820865109b29272bddf1ed0ae63f
duaraghav8 <duaraghav8@gmail.com>
2019-01-26 12:05:10 +0530

    Add rule check-send-result

---
+++ rules/check-send-result.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Enforce checking the result of 'send()' call
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+"use strict";
+
+module.exports = {
+    meta: {
+        docs: {
+            recommended: true,
+            type: "warning",
+            description: "Enforce checking the result of 'send()' call"
+        },
+        schema: []
+    },
+
+    create(context) {
+        return {
+            
+        };
+    }
+};
